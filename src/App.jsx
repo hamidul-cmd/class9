b52e056c5e20aa2a34299a2c54a8586e5961c146
Hamidul Islam Maruf <hamidulislammaruf2@gmail.com>
2025-02-25 23:22:57 +0800

    fdfv

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef, useState } from "react";
+
+function App() {
+  // normal gsap
+
+  // useGSAP(() => {
+  //   gsap.to(".box", {
+  //     x: 1000,
+  //     duration: 2,
+  //     delay: 1,
+  //   });
+  // });
+
+  // gspa with useref
+
+  // const gsapref = useRef();
+  // useGSAP(() => {
+  //   gsap.from(gsapref.current, {
+  //     y: 300,
+  //     rotate: 360,
+  //     opacity:0,
+  //     duration: 2,
+  //     delay: 1,
+  //   });
+  // });
+
+  // gspa with scop
+  // const container = useRef()
+  // useGSAP(() => {
+  //   gsap.to(".box", {
+  //     rotate: 360,
+  //     duration: 2,
+  //     delay: 1,
+  //   });
+  // },{scope:container});
+
+  const [surcal, setsurcal] = useState(0)
+  const rendom = gsap.utils.random(-500,500,100)
+  useGSAP(()=> {
+    gsap.to(".surcal", {
+      
+    })
+  })
+
+  return (
+    <div>
+      <main>
+        <button onClick={()=>{
+          setsurcal(rendom)
+        }}>click</button>
+    <div className="surcal"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
